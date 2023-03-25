4058bacf9c3bbdd3bbacb00098e9167ceaf234b6
HP <HP@DESKTOP-VP5V6ES>
2023-03-26 00:09:56 +0200

    Add project files.

---
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult SubmitTag()
+        {
+            var name = Request.Form["name"];
+            var display = Request.Form["displayName"];
+
+            return View("Add");
+        }
+    }
+}
---
+++ Bloggie.Web/Data/BloggieDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Bloggie.Web.Models.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Data
+{
+    public class BloggieDbContext : IdentityDbContext
+    {
+        public BloggieDbContext(DbContextOptions<BloggieDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
